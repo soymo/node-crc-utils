858c858e8426e7020f96a0c36422bd883de3da36
Jakub Lekstan <kuebzky@gmail.com>
2013-06-17 12:13:39 +0200

    len/crc properties are now returned as uint32

--- index.js
+++ index.js
@@ -1,17 +1,20 @@
 var crc32 = require('./build/default/crc32');
 var compress_buffer_crc32 = require('compress-buffer-crc32');
+var binpack = require('binpack');
 
 exports.compress = function (body) {
     var concatBuff = compress_buffer_crc32.compress(body, -1);
     var len = concatBuff.length;
+
     /*console.log('c', concatBuff, len);
     console.log('d', 10, len - 10);
     console.log('l', len-4, len);
     console.log('s', len-8, len-4);*/
+
     return {
         data: concatBuff.slice(10,len - 10),
-        len: concatBuff.slice(len-4),
-        crc: concatBuff.slice(len-8, len-4)
+        len: binpack.unpackUInt32(concatBuff.slice(len-4), 'little'),
+        crc: binpack.unpackUInt32(concatBuff.slice(len-8, len-4), 'little')
     };
 };
 
